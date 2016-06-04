5adf6e05afd054f32d736632c1e0fb9f891001e5
Harlan Harris <harlan@harris.name>
2016-06-04 14:13:36 -0400

    generate initial state from UI

--- server.R
+++ server.R
@@ -6,18 +6,26 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(ggplot2)
 
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  init_state <- reactive({
+    # generate a data frame of units
+    units <- data.frame(bld_id=c(rep(seq.int(from=1000, length.out=input$num_small), 5),
+                        rep(seq.int(from=2000, length.out=input$num_med), 25),
+                        rep(seq.int(from=3000, length.out=input$num_large), 200)),
+                        rent=1000)
+    units$unit_id <- seq_len(nrow(units))
+    units$yrs_to_turnover <- rnbinom(input$num_small+input$num_med+input$num_large,
+                                     size=input$var_years,
+                                     mu=input$mean_years)
+    units
+  })
+  output$time_in_unit <- renderPlot({
+    ggplot(init_state(), aes(yrs_to_turnover)) + geom_histogram(binwidth=1)
   })
 
 })
--- ui.R
+++ ui.R
@@ -14,13 +14,25 @@ dashboardPage(
   dashboardBody(
     fluidRow(
       column(width=8,
-             box(title="asdf", status="primary", width=NULL, "Graph Goes Here")),
+             box(title="asdf", status="primary", width=NULL, "Graph Goes Here"),
+             box(title="Supporting Graphs", width=NULL,
+                 column(width=6,
+                        plotOutput("time_in_unit", height="200px")),
+                 column(width=6,
+                        plotOutput("final_rent_distribution")))),
       column(width=4,
-             box(title="Housing Stock", width=NULL),
-             box(title="Population", width=NULL),
+             box(title="Housing Stock", width=NULL, collapsible=TRUE,
+                 sliderInput("num_small", "# Small (5)", min=0, max=1000, value=400, step=100),
+                 sliderInput("num_med", "# Medium (25)", min=0, max=1000, value=400, step=100),
+                 sliderInput("num_large", "# Large (200)", min=0, max=1000, value=200, step=100)
+             ),
+             box(title="Population", width=NULL, collapsible=TRUE,
+                 sliderInput("mean_years", "Ave. Years", min=1, max=20, value=5, step=1),
+                 sliderInput("var_years", "Years Variance", min=0, max=100, value=10)),
              box(title="Economics", width=NULL),
              box(title="Policy", width=NULL),
-             actionButton("reset", "Reset"))
+             actionButton("reset", "Reset"),
+             actionButton("run", "Run"))
     )
   ),
   title="Rent Stabilization Policy Simulation"
